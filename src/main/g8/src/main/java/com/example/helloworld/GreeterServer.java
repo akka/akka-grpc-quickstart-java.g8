d2787745040f6d07eee7817fdd09fc93dfcbb9bd
Arnout Engelen <arnout@bzzt.net>
2019-12-20 08:16:00 +0000

    use ConfigFactory.load()

--- src/main/g8/src/main/java/com/example/helloworld/GreeterServer.java
+++ src/main/g8/src/main/java/com/example/helloworld/GreeterServer.java
@@ -36,7 +36,7 @@ class GreeterServer {
   static void main(String[] args) throws Exception {
     // important to enable HTTP/2 in ActorSystem's config
     Config conf = ConfigFactory.parseString("akka.http.server.preview.enable-http2 = on")
-      .withFallback(ConfigFactory.defaultApplication());
+      .withFallback(ConfigFactory.load());
     ActorSystem system = ActorSystem.create("HelloWorld", conf);
     new GreeterServer(system).run();
   }
